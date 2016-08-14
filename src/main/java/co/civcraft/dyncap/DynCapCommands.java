f9e784af4cf05d13ce73189ab46bf02866375ec6
tyrothalos <s.goeppele.parrish@gmail.com>
2016-08-14 16:51:31 -0700

    Reformatted source files.

--- src/main/java/co/civcraft/dyncap/DynCapCommands.java
+++ src/main/java/co/civcraft/dyncap/DynCapCommands.java
@@ -1,4 +1,4 @@
-package com.untamedears.DynCap;
+package co.civcraft.dyncap;
 
 import java.util.logging.Logger;
 
@@ -9,15 +9,16 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class DynCapCommands implements CommandExecutor {
-	
+
 	private DynCapPlugin plugin;
+
 	private Logger log;
-	
+
 	public DynCapCommands(DynCapPlugin p, Logger l) {
 		this.plugin = p;
 		this.log = l;
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -26,12 +27,12 @@ public class DynCapCommands implements CommandExecutor {
 		} else if (label.equalsIgnoreCase("getqueueinfo") || label.equalsIgnoreCase("gqi")) {
 			return getQueueInfo(sender, args);
 		}
-		
+
 		//commands below here can only be issued from the console
 		if (sender instanceof Player) {
 			return false;
 		}
-		
+
 		if (label.equalsIgnoreCase("setCap")) {
 			return setcapCmd(args);
 		} else if (label.equalsIgnoreCase("getCap")) {
@@ -45,9 +46,11 @@ public class DynCapCommands implements CommandExecutor {
 		}
 		return false;
 	}
-	
+
 	private boolean setcapCmd(String[] args) {
-		if (args.length < 1 || args.length > 1) { return false; }
+		if (args.length < 1 || args.length > 1) {
+			return false;
+		}
 		int cap = 1000;
 		try {
 			cap = Integer.parseInt(args[0]);
@@ -57,11 +60,11 @@ public class DynCapCommands implements CommandExecutor {
 		}
 		plugin.setPlayerCap(cap);
 		Integer playerCap = plugin.getPlayerCap();
-		String message = "Dynamic player cap set to: "+playerCap.toString();
+		String message = "Dynamic player cap set to: " + playerCap.toString();
 		log.info(message);
 		return true;
 	}
-	
+
 	private boolean getcapCmd() {
 		Integer playerCap = plugin.getPlayerCap();
 		Integer playerCount = plugin.getPlayerCount();
@@ -69,124 +72,101 @@ public class DynCapCommands implements CommandExecutor {
 		log.info(message);
 		return true;
 	}
-	
-	private boolean getQueueSize(CommandSender sender) 
-	{
+
+	private boolean getQueueSize(CommandSender sender) {
 		Integer queueSize = plugin.getQueueSize();
 		return sendMessage(sender, queueSize + " players are in the queue.");
 	}
 
-	private boolean reloadQueueConfig() 
-	{
+	private boolean reloadQueueConfig() {
 		plugin.reloadConfig();
 		plugin.initConfig();
 		log.info("queue config reloaded");
 		return true;
 	}
-	
-	private boolean getQueueInfo(CommandSender sender, String[] args) 
-	{
-		if (!sender.hasPermission("dyncap.debug.getqueueinfo"))
-		{
+
+	private boolean getQueueInfo(CommandSender sender, String[] args) {
+		if (!sender.hasPermission("dyncap.debug.getqueueinfo")) {
 			return false;
 		}
-		if (args.length < 1 || args.length > 2) { return false; }
-		
-		if (args.length == 1)
-		{
+		if (args.length < 1 || args.length > 2) {
+			return false;
+		}
+
+		if (args.length == 1) {
 			//display info on 1 slot
-			if (isInteger(args[0]))
-			{
+			if (isInteger(args[0])) {
 				int startIndex = (Integer.parseInt(args[0]) - 1);
 				QueueItem queueItem = plugin.getQueueItem(startIndex);
-				if (queueItem != null)
-				{
-					return sendMessage(sender, startIndex + "     " + queueItem.getName() + "     " + queueItem.getSecondsSinceLastAttempt());
-				}
-				else
-				{
+				if (queueItem != null) {
+					return sendMessage(sender, startIndex + "     " + queueItem.getName() + "     " + queueItem
+							.getSecondsSinceLastAttempt());
+				} else {
 					return sendMessage(sender, "Could not find information about index " + args[0]);
 				}
 			}
 			//search by name
-			else
-			{
+			else {
 				String playerName = args[0];
 				int index = plugin.getQueuePosition(playerName);
-				if (index != -1)
-				{
+				if (index != -1) {
 					QueueItem queueItem = plugin.getQueueItem(index);
-					return sendMessage(sender, index + "     " + queueItem.getName() + "     " + queueItem.getSecondsSinceLastAttempt() );
-				}
-				else
-				{
+					return sendMessage(sender, index + "     " + queueItem.getName() + "     " + queueItem
+							.getSecondsSinceLastAttempt());
+				} else {
 					return sendMessage(sender, "Could not find " + playerName + " in the queue.");
 				}
 			}
-		}
-		else if (args.length == 2)
-		{
-			if (isInteger(args[0]) && isInteger(args[1]))
-			{
+		} else if (args.length == 2) {
+			if (isInteger(args[0]) && isInteger(args[1])) {
 				int startIndex = (Integer.parseInt(args[0]) - 1);
 				int endIndex = (Integer.parseInt(args[1]) - 1);
-				for (int x = startIndex; x <= endIndex; x++)
-				{
+				for (int x = startIndex; x <= endIndex; x++) {
 					QueueItem queueItem = plugin.getQueueItem(x);
-					if (queueItem != null)
-					{
-						sendMessage(sender, x + "     " + queueItem.getName() + "     " + queueItem.getSecondsSinceLastAttempt() );
-					}
-					else
-					{
+					if (queueItem != null) {
+						sendMessage(sender, x + "     " + queueItem.getName() + "     " + queueItem
+								.getSecondsSinceLastAttempt());
+					} else {
 						sendMessage(sender, "Could not find information about index " + (x + 1));
-					}				
+					}
 				}
 				return true;
-			}
-			else
-			{
+			} else {
 				sendMessage(sender, "Please enter two integers above 0");
 			}
 		}
 		return false;
 	}
-	
-	private static boolean isInteger(String s) {
-	    try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    }
-	    // only got here if we didn't return false
-	    return true;
-	}
-	
 
-	private boolean resetJoinAverage() 
-	{
+	private static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		// only got here if we didn't return false
+		return true;
+	}
+
+
+	private boolean resetJoinAverage() {
 		plugin.resetAverageTimeToJoin();
 		log.info("Average join time has been reset!");
 		return true;
 	}
 
-	private boolean getJoinAverage() 
-	{
+	private boolean getJoinAverage() {
 		float average = plugin.getAverageTimeToJoin();
 		log.info("Average join time is " + average + " seconds.");
 		return true;
 	}
-	
-	private boolean sendMessage(CommandSender sender, String string)
-	{
-		if (sender instanceof Player)
-		{
+
+	private boolean sendMessage(CommandSender sender, String string) {
+		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			player.sendMessage(string);
 			return true;
-		}
-		else if (sender instanceof ConsoleCommandSender)
-		{
+		} else if (sender instanceof ConsoleCommandSender) {
 			log.info(string);
 			return true;
 		}
--- src/main/java/co/civcraft/dyncap/DynCapPlugin.java
+++ src/main/java/co/civcraft/dyncap/DynCapPlugin.java
@@ -1,4 +1,5 @@
-package com.untamedears.DynCap;
+package co.civcraft.dyncap;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -16,24 +17,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class DynCapPlugin extends JavaPlugin implements Listener {
+
 	private DynCapCommands commands;
+
 	private int dynamicPlayerCap = 175;
+
 	private Logger log;
+
 	private float averageTimeToJoin = 0F;
+
 	private int numberOfJoins = 0;
-	private List <QueueItem> loginQueue = new ArrayList<QueueItem>();
+
+	private List<QueueItem> loginQueue = new ArrayList<QueueItem>();
+
 	private String firstJoinMessage;
+
 	private String updateMessage;
+
 	private String toFastJoinMessage;
+
 	private String banMessage;
+
 	private int minimumJoinTime;
+
 	private int timeOutTime;
+
 	private Set<String> whiteListedPlayers;
-	
+
 	public void onEnable() {
 		log = this.getLogger();
 		commands = new DynCapCommands(this, log);
-		
+
 		Bukkit.getPluginManager().registerEvents(this, this);
 		for (String command : getDescription().getCommands().keySet()) {
 			getCommand(command).setExecutor(commands);
@@ -42,38 +56,43 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 		// Give the console permission
 		ConsoleCommandSender console = getServer().getConsoleSender();
 		console.addAttachment(this, "dyncap.console", true);
-				
+
 		this.saveDefaultConfig();
 
 		initConfig();
-		
-		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
-		{
-		    @Override  
-		    public void run() 
-		    {
-		    	removeOldQueueItems(timeOutTime);
-		    }}, 0L, 100);		
+
+		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+
+			@Override
+			public void run() {
+				removeOldQueueItems(timeOutTime);
+			}
+		}, 0L, 100);
 	}
 
-	public void onDisable() {}
-	
-	public void initConfig()
-	{
+	public void onDisable() {
+	}
+
+	public void initConfig() {
 		FileConfiguration config = getConfig();
-		firstJoinMessage = config.getString("messages.firstJoin", "The server is full, you have been added to the login queue. Your current position is %d. Please try again in no less than 10 seconds, and no more than 60 seconds.");
-		toFastJoinMessage = config.getString("messages.toFastJoin", "You rejoined way to quick, you have been moved to the end of the login queue. Next time please join no less than every 10 seconds.");
-		updateMessage = config.getString("messages.updateMessage", "Your posistion in the queue is %d. Please try again in no less than 10 seconds, and no more than 60 seconds.");
-		banMessage = config.getString("messages.banMessage", "You are banned. If you believe this is a mistake, message modmail at www.reddit.com/r/civcraft");
+		firstJoinMessage = config.getString("messages.firstJoin", "The server is full, you have been added to the " +
+				"login queue. Your current position is %d. Please try again in no less than 10 seconds, and no more " +
+				"than 60 seconds.");
+		toFastJoinMessage = config.getString("messages.toFastJoin", "You rejoined way to quick, you have been moved " +
+				"to" +
+				" the end of the login queue. Next time please join no less than every 10 seconds.");
+		updateMessage = config.getString("messages.updateMessage", "Your posistion in the queue is %d. Please try " +
+				"again in no less than 10 seconds, and no more than 60 seconds.");
+		banMessage = config.getString("messages.banMessage", "You are banned. If you believe this is a mistake, " +
+				"message modmail at www.reddit.com/r/civcraft");
 		minimumJoinTime = config.getInt("timers.minimumJoinTime", 5);
 		timeOutTime = config.getInt("timers.timeOutTime", 60);
 		dynamicPlayerCap = config.getInt("other.initialDynCap", 175);
 		whiteListedPlayers = config.getConfigurationSection("whiteListedPlayers").getKeys(false);
 	}
-	
+
 	@EventHandler
-	public void onServerListPingEvent(ServerListPingEvent event) 
-	{
+	public void onServerListPingEvent(ServerListPingEvent event) {
 		event.setMaxPlayers(getPlayerCap());
 	}
 
@@ -89,55 +108,44 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 	public int getPlayerCount() {
 		return this.getServer().getOnlinePlayers().size();
 	}
-	
-	public int getQueueSize()
-	{
+
+	public int getQueueSize() {
 		return loginQueue.size();
 	}
-	
-	public synchronized QueueItem getQueueItem(int queueIndex)
-	{
-		if (queueIndex < 0 || queueIndex >= loginQueue.size())
-		{
+
+	public synchronized QueueItem getQueueItem(int queueIndex) {
+		if (queueIndex < 0 || queueIndex >= loginQueue.size()) {
 			return null;
-		}
-		else
-		{
+		} else {
 			return loginQueue.get(queueIndex);
 		}
 	}
-		
-	@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled = false)
-	public void onAsyncPlayerPreLoginEvent(AsyncPlayerPreLoginEvent event) 
-	{
-		try
-		{
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
+	public void onAsyncPlayerPreLoginEvent(AsyncPlayerPreLoginEvent event) {
+		try {
 			//log.info("login event called!");
 			String playerName = event.getName().toLowerCase();
-			
+
 			//if the player is whitelisted(admin/mod)
-			if (whiteListedPlayers.contains(playerName))
-			{
+			if (whiteListedPlayers.contains(playerName)) {
 				event.allow();
 				return;
 			}
-			
+
 			int position = getQueuePosition(playerName);
 			//log.info("posistion is:" + position);
 			//if the server is not full, and there is no queue
-			if ((getPlayerCount() < getPlayerCap() && loginQueue.isEmpty()))
-			{
+			if ((getPlayerCount() < getPlayerCap() && loginQueue.isEmpty())) {
 				//log.info("allowed " + playerName + " to join, server is not full and has no queue!");
 				event.allow();
 				updateAverageTimeToJoin(0);
 				return;
 			}
 			//server is either full, or has a queue
-			else
-			{
+			else {
 				//if the server has a queue, but there is enough space for the player
-				if(position != -1 && position + 1 <= getPlayerCap() - getPlayerCount())
-				{
+				if (position != -1 && position + 1 <= getPlayerCap() - getPlayerCount()) {
 					//log.info("allowed " + playerName + " to join, server is not full and he is in a queue!");
 					event.allow();
 					updateAverageTimeToJoin(loginQueue.get(position).getSecondsSinceFirstAttempt());
@@ -145,28 +153,21 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 					return;
 				}
 				//if the server has a queue, and there is not enough space for the player
-				else if (position != -1)
-				{
-					if (getQueueItem(position).getSecondsSinceLastAttempt() <= minimumJoinTime)
-					{
+				else if (position != -1) {
+					if (getQueueItem(position).getSecondsSinceLastAttempt() <= minimumJoinTime) {
 						removeFromQueue(position);
 						event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, toFastJoinMessage);
 						return;
+					} else {
+						getQueueItem(position).updateDate();
+						event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, String.format(updateMessage,
+								(position + 1)));
+						return;
 					}
-					else
-					{
-					getQueueItem(position).updateDate();
-					event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, String.format(updateMessage, (position + 1)));
-					return;
-					}
-				}
-				else
-				{
-					if ((!loginQueue.isEmpty() && loginQueue.size() <= (getPlayerCap() - getPlayerCount())))
-					{
+				} else {
+					if ((!loginQueue.isEmpty() && loginQueue.size() <= (getPlayerCap() - getPlayerCount()))) {
 						//if for some reason the person is in the queue remove them
-						if (position != -1)
-						{
+						if (position != -1) {
 							removeFromQueue(position);
 						}
 						updateAverageTimeToJoin(0);
@@ -176,85 +177,72 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 					//log.info("disallowed " + playerName + " added him to queue");
 					QueueItem queueItem = new QueueItem(playerName);
 					addToQueue(queueItem);
-					event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, String.format(firstJoinMessage, loginQueue.size()));
+					event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_FULL, String.format(firstJoinMessage,
+							loginQueue.size()));
 					return;
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private synchronized void removeFromQueue(int index)
-	{
+
+	private synchronized void removeFromQueue(int index) {
 		loginQueue.remove(index);
 	}
-	private synchronized void addToQueue(QueueItem queueItem)
-	{
+
+	private synchronized void addToQueue(QueueItem queueItem) {
 		loginQueue.add(queueItem);
 	}
+
 	//returning -1  means error/not contained
-	public synchronized int getQueuePosition(String name)
-	{
-		if (loginQueue.isEmpty())
-		{
+	public synchronized int getQueuePosition(String name) {
+		if (loginQueue.isEmpty()) {
 			//log.info("queue is empty!");
 			return -1;
 		}
-		for (int x = 0; x < loginQueue.size(); x++)
-		{
-			//log.info("x is:" + x + " and queueItem name is " + queue.get(x).getName() + " while paramter is " + name);
-			if (getQueueItem(x).getName().equalsIgnoreCase(name))
-			{
+		for (int x = 0; x < loginQueue.size(); x++) {
+			//log.info("x is:" + x + " and queueItem name is " + queue.get(x).getName() + " while paramter is " +
+			// name);
+			if (getQueueItem(x).getName().equalsIgnoreCase(name)) {
 				return x;
 			}
 		}
 		return -1;
-	}	
-	
-	
-	public void updateAverageTimeToJoin(int newJoinTime)
-	{
+	}
+
+
+	public void updateAverageTimeToJoin(int newJoinTime) {
 		numberOfJoins++;
 		Float tempFloat = Integer.valueOf(numberOfJoins).floatValue();
 		Float tempFloat2 = Integer.valueOf(newJoinTime).floatValue();
-		averageTimeToJoin = ((tempFloat -1F)/tempFloat) * averageTimeToJoin + (tempFloat2/tempFloat);
+		averageTimeToJoin = ((tempFloat - 1F) / tempFloat) * averageTimeToJoin + (tempFloat2 / tempFloat);
 	}
-	
-	public void resetAverageTimeToJoin()
-	{
+
+	public void resetAverageTimeToJoin() {
 		averageTimeToJoin = 0F;
 		numberOfJoins = 0;
 	}
-	
-	public float getAverageTimeToJoin()
-	{
+
+	public float getAverageTimeToJoin() {
 		return averageTimeToJoin;
 	}
-	
+
 	//timeOut is in seconds
-	private synchronized void removeOldQueueItems(int timeOut)
-	{
-		if (loginQueue.isEmpty())
-		{
+	private synchronized void removeOldQueueItems(int timeOut) {
+		if (loginQueue.isEmpty()) {
 			return;
 		}
-		for (int x = 0; x< loginQueue.size(); x++)
-		{
-			if (loginQueue.get(x).getSecondsSinceLastAttempt() > timeOut)
-			{
+		for (int x = 0; x < loginQueue.size(); x++) {
+			if (loginQueue.get(x).getSecondsSinceLastAttempt() > timeOut) {
 				removeFromQueue(x);
-				x -- ;
+				x--;
 			}
 		}
 	}
-	
-	private void moveQueueItem(int oldIndex, int newIndex)
-	{
-		if (oldIndex < 0 || oldIndex >= loginQueue.size() || newIndex < 0 || newIndex >= loginQueue.size())
-		{
+
+	private void moveQueueItem(int oldIndex, int newIndex) {
+		if (oldIndex < 0 || oldIndex >= loginQueue.size() || newIndex < 0 || newIndex >= loginQueue.size()) {
 			return;
 		}
 		QueueItem tempItem = loginQueue.get(oldIndex);
